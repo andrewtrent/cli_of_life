dea206bf98f5cf6b1523d2ef998f01f4d65003cb
Andrew Trent <andrewtrentdesign@gmail.com>
2019-04-18 00:19:35 +0000

    Basic object structure for taxa levels.

--- cli_of_life/lib/cli_of_life/taxa.rb
+++ cli_of_life/lib/cli_of_life/taxa.rb
@@ -1,3 +1,13 @@
-module CliOfLife
-  
+require 'pry'
+module CliOfLife::Taxa
+  class Taxa
+    attr_accessor :taxa_level, :taxa_definition, :taxa_contents
+    def initialize(level, definition, contents)
+      @taxa_level = level
+      @taxa_definition = definition
+      @taxa_contents = contents
+    end
+  end
+
 end
+animalia = CliOfLife::Taxa::Taxa.new("Kingdom", "Animals make cholesterol and stuff", ["Mollusca", "Chordata", "Nematoda", "Annelida"])
